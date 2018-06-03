2fa7a27347a74800ba0abbc4f63adf482b99f272
HarveyYesan <yuhaiyong@foxmail.com>
2018-06-03 21:23:39 +0800

    Code for SVM and Spam email classification in practice
    Signed-off-by: HarveyYesan <yuhaiyong@foxmail.com>

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -23,11 +23,30 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+steps = [0.01 0.03 0.1 0.3 1 3 10 30];
+optC = Inf;
+optSIG = Inf;
+minErr = Inf;
 
+for i = 1:length(steps)
+  for j = 1:length(steps)
+    currentC = steps(i);
+    currentSIG = steps(j);
+    model = svmTrain(X, y, currentC, @(x1, x2) gaussianKernel(x1, x2, currentSIG));
+    predict = svmPredict(model, Xval);
+    err = mean(double(predict ~= yval));
+    if (err < minErr)
+      minErr = err;
+      optC = currentC;
+      optSIG = currentSIG;
+    end
+  end
+end    
 
+C = optC;
+sigma = optSIG;
 
-
-
+fprintf("%d, %d\n", C, sigma);
 
 % =========================================================================
 
